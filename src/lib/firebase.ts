3dccdaa71f40cc3b1917af8d1cbc687c1104d369
AYUSHTRIPATHI0 <tripathiayush944@gmail.com>
2025-08-15 02:37:37 +0530

    Moved Firebase key to environment variable

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  "projectId": "healthlink-iymsz",
-  "appId": "1:87432400493:web:14845235939d74a2e77964",
-  "storageBucket": "healthlink-iymsz.appspot.com",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "healthlink-iymsz.firebaseapp.com",
-  "messagingSenderId": "87432400493",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
   "measurementId": ""
 };
 
